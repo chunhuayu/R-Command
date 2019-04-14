9df61136f9d4e9253c550554426350c0e4f5e12f
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-04-14 16:34:21 -0400

    Update solve the equation.r

--- solve the equation.r
+++ solve the equation.r
@@ -1,2 +1,5 @@
-## solve the equation Ax=b ###
+### solve the equation Ax=b ###
 solve(A,b)
+### p(x) = z1 + z2 * x + … + z[n] * x^(n-1) ,c(z1,z2,z3,...,z(n))###
+> polyroot(c(1,2,1))
+[1] -1-0i -1+0i
