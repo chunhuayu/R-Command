7bd55beaa85e0bae9840883f642d0f3d619d08d3
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-04-23 23:05:02 -0400

    Create Fit Model.r

---
+++ Fit Model.r
@@ -0,0 +1,7 @@
+## The main model-fitting commands covered on this page are:
+
+## lm (linear models for fixed effects)
+## lme (linear models for mixed effects)
+## glm (generalized linear models)
+## nls (nonlinear least squares)
+## gam (generalized additive models)
