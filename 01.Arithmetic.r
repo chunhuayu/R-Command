2dd244e836199e55dfdab41f6d771a4583baf727
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-07 11:01:16 -0400

    Create 01.Arithmetic.r

---
+++ 01.Arithmetic.r
@@ -0,0 +1,38 @@
+# Arithmetic with R
+# In its most basic form, R can be used as a simple calculator. Consider the following arithmetic operators:
+
+# Addition: +
+# Subtraction: -
+# Multiplication: *
+# Division: /
+# Exponentiation: ^
+# Modulo: %%
+# The last two might need some explaining:
+
+# The ^ operator raises the number to its left to the power of the number to its right: for example 3^2 is 9.
+# The modulo returns the remainder of the division of the number to the left by the number on its right, 
+# for example 5 modulo 3 or 5 %% 3 is 2.
+
+> # An addition
+> 5 + 5
+[1] 10
+> 
+> # A subtraction
+> 5 - 5
+[1] 0
+> 
+> # A multiplication
+> 3 * 5
+[1] 15
+> 
+>  # A division
+> (5 + 5) / 2
+[1] 5
+> 
+> # Exponentiation
+> 2^5
+[1] 32
+> 
+> # Modulo
+> 28%%6
+[1] 4
