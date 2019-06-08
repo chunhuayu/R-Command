90c985ecd49bd03a935d9e1cc112646c48e28072
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-08 01:58:47 -0400

    Update 502 Creating a data frame.r

--- 01 BASICS/502 Creating a data frame.r
+++ 01 BASICS/502 Creating a data frame.r
@@ -1,5 +1,5 @@
 ### Creating a data frame
-
+# (1)
 # Since using built-in data sets is not even half the fun of creating your own data sets, 
 # the rest of this chapter is based on your personally developed data set. 
 # Put your jet pack on because it is time for some space exploration!
@@ -21,12 +21,21 @@
 # Because every column has the same length, the vectors you pass should also have the same length. 
 # But don't forget that it is possible (and likely) that they contain different types of data.
 
+# (2)
+# The planets_df data frame should have 8 observations and 5 variables. 
+# It has been made available in the workspace, so you can directly use it.
+
+
 ### INSTRUCTIONS
+# (1)
 # Use the function data.frame() to construct a data frame. 
 # Pass the vectors name, type, diameter, rotation and rings as arguments to data.frame(), in this order.
 # Call the resulting data frame planets_df.
+# (2)
+# Use str() to investigate the structure of the new planets_df variable.
 
 ### R SCRIPT
+# (1)
 > # Definition of vectors
 > name <- c("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
 > type <- c("Terrestrial planet", "Terrestrial planet", "Terrestrial planet", 
@@ -47,4 +56,14 @@
 6  Saturn          Gas giant    9.449     0.43  TRUE
 7  Uranus          Gas giant    4.007    -0.72  TRUE
 8 Neptune          Gas giant    3.883     0.67  TRUE
+> 
+# (2)
+> # Check the structure of planets_df
+> str(planets_df)
+'data.frame':	8 obs. of  5 variables:
+ $ name    : Factor w/ 8 levels "Earth","Jupiter",..: 4 8 1 3 2 6 7 5
+ $ type    : Factor w/ 2 levels "Gas giant","Terrestrial planet": 2 2 2 2 1 1 1 1
+ $ diameter: num  0.382 0.949 1 0.532 11.209 ...
+ $ rotation: num  58.64 -243.02 1 1.03 0.41 ...
+ $ rings   : logi  FALSE FALSE FALSE FALSE TRUE TRUE ...
 > 
