e4a8c7e1f5649576b71b06eff89674a0095cf9d2
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-09 19:36:15 -0400

    Create 305 Funtions-if-else.r

---
+++ 02 Intermediate R/305 Funtions-if-else.r
@@ -0,0 +1,41 @@
+### function
+# Now that you've acquired some skills in defining functions with different types of arguments and return values, 
+# you should try to create more advanced functions. As you've noticed in the previous exercises, 
+# it's perfectly possible to add control-flow constructs, loops and even other functions to your function body.
+
+# Remember our social media example? 
+# The vectors linkedin and facebook are already defined in the workspace so you can get your hands dirty straight away. 
+# As a first step, you will be writing a function that can interpret a single value of this vector. 
+# In the next exercise, you will write another function that can handle an entire vector at once.
+
+### INSTRUCTIONS
+# Finish the function definition for interpret(), that interprets the number of profile views on a single day:
+# The function takes one argument, num_views.
+# If num_views is greater than 15, the function prints out "You're popular!" to the console and returns num_views.
+# Else, the function prints out "Try to be more visible!" and returns 0.
+# Finally, call the interpret() function twice: 
+# on the first value of the linkedin vector and on the second element of the facebook vector.
+
+### R
+> # The linkedin and facebook vectors have already been created for you
+> 
+> # Define the interpret function
+> interpret <- function(num_views) {
+    if (num_views > 15) {
+  print("You're popular!")
+  return(num_views)
+    } else {
+  print("Try to be more visible!")
+  return(0)
+    }
+  }
+> 
+> # Call the interpret function twice
+> interpret(linkedin)
+Warning message: the condition has length > 1 and only the first element will be used
+[1] "You're popular!"
+[1] 16  9 13  5  2 17 14
+> interpret(facebook[2])
+[1] "Try to be more visible!"
+[1] 0
+> 
