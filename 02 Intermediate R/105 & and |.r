f8772f764ce3d650253a147143b912b02b868c0c
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-08 23:10:39 -0400

    Update 105 & and |.r

--- 02 Intermediate R/105 & and |.r
+++ 02 Intermediate R/105 & and |.r
@@ -15,11 +15,18 @@ FALSE | TRUE
 # Both the variables linkedin and last have already been defined in the editor.
 
 ### INSTRUCTIONS
+# (1)
 # Write R expressions to solve the following questions concerning the variable last:
 # Is last under 5 or above 10?
 # Is last between 15 and 20, excluding 15 but including 20?
+# (2)
+# When did LinkedIn views exceed 10 and did Facebook views fail to reach 10 for a particular day? Use the linkedin and facebook vectors.
+# When were one or both of your LinkedIn and Facebook profiles visited at least 12 times?
+# When is the views matrix equal to a number between 11 and 14, excluding 11 and including 14?
+
 
 ### R
+# (1)
 > # The linkedin and last variable are already defined for you
 > linkedin <- c(16, 9, 13, 5, 2, 17, 14)
 > last <- tail(linkedin, 1)
@@ -32,3 +39,22 @@ FALSE | TRUE
 > last <= 20 & last >15
 [1] FALSE
 > 
+# (2)
+
+> # The social data (linkedin, facebook, views) has been created for you
+> 
+> # linkedin exceeds 10 but facebook below 10
+> linkedin >10 & facebook <10
+[1] FALSE FALSE  TRUE FALSE FALSE FALSE FALSE
+> 
+> # When were one or both visited at least 12 times?
+> linkedin >=12 | facebook >=12
+[1]  TRUE FALSE  TRUE  TRUE FALSE  TRUE  TRUE
+> 
+> # When is views between 11 (exclusive) and 14 (inclusive)?
+> views <= 14 & views >11
+      [,1]  [,2]  [,3]  [,4]  [,5]  [,6] [,7]
+[1,] FALSE FALSE  TRUE FALSE FALSE FALSE TRUE
+[2,] FALSE FALSE FALSE FALSE FALSE  TRUE TRUE
+> 
+
