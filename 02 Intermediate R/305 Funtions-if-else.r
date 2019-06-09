8fe9db308be9587c34699d181889a5f672d90a31
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-09 19:43:28 -0400

    Update 305 Funtions-if-else.r

--- 02 Intermediate R/305 Funtions-if-else.r
+++ 02 Intermediate R/305 Funtions-if-else.r
@@ -9,6 +9,7 @@
 # In the next exercise, you will write another function that can handle an entire vector at once.
 
 ### INSTRUCTIONS
+# (1)
 # Finish the function definition for interpret(), that interprets the number of profile views on a single day:
 # The function takes one argument, num_views.
 # If num_views is greater than 15, the function prints out "You're popular!" to the console and returns num_views.
@@ -16,7 +17,19 @@
 # Finally, call the interpret() function twice: 
 # on the first value of the linkedin vector and on the second element of the facebook vector.
 
+# (2)
+# Finish the template for the interpret_all() function:
+# Make return_sum an optional argument, that is TRUE by default.
+# Inside the for loop, iterate over all views: on every iteration, add the result of interpret(v) to count. Remember that interpret(v) returns v for popular days, and 0 otherwise. At the same time, interpret(v) will also do some printouts.
+# Finish the if construct:
+# If return_sum is TRUE, return count.
+# Else, return NULL.
+# Call this newly defined function on both linkedin and facebook.
+
+
+
 ### R
+# (1)
 > # The linkedin and facebook vectors have already been created for you
 > 
 > # Define the interpret function
@@ -39,3 +52,56 @@ Warning message: the condition has length > 1 and only the first element will be
 [1] "Try to be more visible!"
 [1] 0
 > 
+
+# (2)
+> # The linkedin and facebook vectors have already been created for you
+> linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+> facebook <- c(17, 7, 5, 16, 8, 13, 14)
+> 
+> # The interpret() can be used inside interpret_all()
+> interpret <- function(num_views) {
+    if (num_views > 15) {
+      print("You're popular!")
+      return(num_views)
+    } else {
+      print("Try to be more visible!")
+      return(0)
+    }
+  }
+> 
+> # Define the interpret_all() function
+> # views: vector with data to interpret
+> # return_sum: return total number of views on popular days?
+> interpret_all <- function(views, return_sum =TRUE) {
+    count <- 0
+  
+    for (v in views) {
+  count = interpret(v)+count
+    }
+  
+    if (return_sum) {
+  return(count)
+    } else {
+  return(NULL)
+    }
+  }
+> 
+> # Call the interpret_all() function on both linkedin and facebook
+> interpret_all(linkedin)
+[1] "You're popular!"
+[1] "Try to be more visible!"
+[1] "Try to be more visible!"
+[1] "Try to be more visible!"
+[1] "Try to be more visible!"
+[1] "You're popular!"
+[1] "Try to be more visible!"
+[1] 33
+> interpret_all(facebook)
+[1] "You're popular!"
+[1] "Try to be more visible!"
+[1] "Try to be more visible!"
+[1] "You're popular!"
+[1] "Try to be more visible!"
+[1] "Try to be more visible!"
+[1] "Try to be more visible!"
+[1] 33
