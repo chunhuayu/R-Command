5d1ba90abf1bd5d6959826cffbede64c148a6069
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-09 09:23:51 -0400

    Create 201 While Loop.r

---
+++ 02 Intermediate R/201 While Loop.r
@@ -0,0 +1,20 @@
+Write a while loop
+
+Let's get you started with building a while loop from the ground up. Have another look at its recipe:
+
+while (condition) {
+  expr
+}
+Remember that the condition part of this recipe should become FALSE at some point during the execution. Otherwise, the while loop will go on indefinitely. In DataCamp's learning interface, your session will be disconnected in this case.
+
+Have a look at the code on the right; it initializes the speed variables and already provides a while loop template to get you started.
+
+Instructions
+100 XP
+Instructions
+100 XP
+Code a while loop with the following characteristics:
+
+The condition of the while loop should check if speed is higher than 30.
+Inside the body of the while loop, print out "Slow down!".
+Inside the body of the while loop, decrease the speed by 7 units. This step is crucial; otherwise your while loop will never stop.
