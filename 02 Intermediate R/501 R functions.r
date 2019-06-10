69712f4552c721b36ee035d7f2891ee06db7b5c6
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-10 00:51:17 -0400

    Create 501 R functions.r

---
+++ 02 Intermediate R/501 R functions.r
@@ -0,0 +1,39 @@
+abs()
+# Calculate the absolute value.
+
+sum() 
+# Calculate the sum of all the values in a data structure.
+
+mean() 
+# Calculate the arithmetic mean.
+
+round() 
+# Round the values to 0 decimal places by default. 
+# Try out ?round in the console for variations of round() and ways to change the number of digits to round to.
+
+seq() 
+# Generate sequences, by specifying the from, to, and by arguments.
+
+rep()
+# Replicate elements of vectors and lists.
+
+sort() 
+# Sort a vector in ascending order. Works on numerics, but also on character strings and logicals.
+
+rev()
+# Reverse the elements in a data structures for which reversal is defined.
+
+str()
+# Display the structure of any R object.
+
+append()
+# Merge vectors or lists.
+
+is.*() 
+# Check for the class of an R object.
+
+as.*()
+# Convert an R object from one class to another.
+
+unlist() 
+# Flatten (possibly embedded) lists to produce a vector.
