7d05684d7b86e6c0839123e154d8b2e24948dc8d
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-11 17:14:52 -0400

    Create 0302 Aesthetics limitation.r

---
+++ 03 ggplot2/0302 Aesthetics limitation.r
@@ -0,0 +1,18 @@
+# Many of the aesthetics can be mapped onto continuous or categorical variables, but some are restricted to categorical data. 
+# Which aesthetics are they?
+
+### Possible Answers
+# 1. color & fill
+# 2. alpha & size
+# 3. label & shape
+# 4. alpha & label
+# 5. x & y
+
+### Answer: 3
+
+
+### These aesthetics can be used for categorical variables and continuous variables: 
+alpha
+size
+color
+fill
