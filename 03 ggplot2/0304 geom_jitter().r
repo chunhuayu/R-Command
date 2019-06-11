9caa380c641949caf85dfb4eaa8325d511a6f152
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-11 19:03:42 -0400

    Update 0304 geom_jitter().r

--- 03 ggplot2/0304 geom_jitter().r
+++ 03 ggplot2/0304 geom_jitter().r
@@ -1,15 +1,42 @@
-# Setting a dummy aesthetic
+### geom_jitter() 
 
-# In the last chapter you saw that all the visible aesthetics can serve as attributes and aesthetics, 
-# but I very conveniently left out x and y. 
-# That's because although you can make univariate plots (such as histograms, 
-# which you'll get to in the next chapter), a y-axis will always be provided, even if you didn't ask for it.
+# A geom that draws a point defined by an x and y coordinate, like geom_point, but jitters the points.
+# Default statistic: stat_identity
+# Default position adjustment: position_jitter
 
-# In the base package you can make univariate plots with stripchart()
-# (shown in the viewer) directly and it will take care of a fake y axis for us. 
-# Since this is univariate data, there is no real y axis.
+### Parameters
+# x - (required) x coordinate of the point
+# y - (required) y coordinate of the point
+# size - (default: 0.5) diameter of the point
+# shape - (default: 16=dot) the shape of the point
+# colour - (default: "black") the color of the point
+# fill - (default: NA) the fill of the point (only a small minority of shapes actually can be filled; see shape)
+# alpha - (default: 1=opaque) the transparency of the point
+# na.rm - (default: FALSE) silently remove points with NA coordinates
 
-# You can get the same thing in ggplot2, but it's a bit more cumbersome. 
+### Case Study I : 
+
+d=data.frame(system=rep(c('before','after'), 2000), runtime=rexp(4000, 1))
+ggplot() + 
+geom_jitter(data=d, mapping=aes(x=system, y=runtime), size=1) +
+opts(title="geom_jitter", plot.title=theme_text(size=40, vjust=1.5))
+# Note: To adjust the amount of jittering along the x and y axes, one can explicitly set the position adjustment:
+
+geom_jitter(data=d, mapping=aes(x=system, y=runtime), size=1, position=position_jitter(width=0.3, height=0)) 
+
+# Because the only difference between geom_jitter and geom_point is the automatically set position adjustment, 
+# the geom_point(...) layer below is identical to the geom_jitter(...) layer above.
+
+geom_point(data=d, mapping=aes(x=system, y=runtime), size=1, position=position_jitter(width=0.3, height=0)) 
+
+
+
+### Case Study II : Setting a dummy aesthetic
+
+# In the base package you can make univariate plots with stripchart() (shown in the viewer) directly 
+# and it will take care of a fake y axis for us. Since this is univariate data, there is no real y axis.
+
+# You can get the same thing in ggplot2, but it's a bit more cumbersome 笨重. 
 # The only reason you'd really want to do this is if you were making many plots and you wanted them to be in the same style,
 # or you wanted to take advantage of an aesthetic mapping (e.g. colour).
 
