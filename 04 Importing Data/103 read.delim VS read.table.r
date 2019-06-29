21562f32ec508f39cde790075314615b4c4e35f8
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-28 22:31:07 -0400

    Create 103 read.delim VS read.table.r

---
+++ 04 Importing Data/103 read.delim VS read.table.r
@@ -0,0 +1,67 @@
+### (1) read.delim
+
+# Aside from .csv files, there are also the .txt files which are basically text files. 
+# You can import these functions with read.delim(). By default, it sets the sep argument to "\t" 
+# (fields in a record are delimited by tabs) and the header argument to TRUE (the first row contains the field names).
+
+# In this exercise, you will import hotdogs.txt, 
+# containing information on sodium and calorie levels in different hotdogs (Source: UCLA). 
+# The dataset has 3 variables, but the variable names are not available in the first line of the file. 
+# The file uses tabs as field separators.
+
+### (2) read.table
+
+# If you're dealing with more exotic flat file formats, you'll want to use read.table(). 
+# It's the most basic importing function; you can specify tons of different arguments in this function. 
+# Unlike read.csv() and read.delim(), the header argument defaults to FALSE and the sep argument is "" by default.
+
+# Up to you again! The data is still hotdogs.txt. It has no column names in the first row, and the field separators are tabs. 
+# This time, though, the file is in the data folder inside your current working directory. 
+# A variable path with the location of this file is already coded for you.
+
+
+### Instructions
+# (1)
+# Import the data in "hotdogs.txt" with read.delim(). Call the resulting data frame hotdogs. 
+# The variable names are not on the first line, so make sure to set the header argument appropriately.
+# Call summary() on hotdogs. This will print out some summary statistics about all variables in the data frame.
+
+# (2)
+# Finish the read.table() call that's been prepared for you. Use the path variable, and make sure to set sep correctly.
+# Call head() on hotdogs; this will print the first 6 observations in the data frame.
+
+
+### R
+# (1)
+> # Import hotdogs.txt: hotdogs
+> hotdogs=read.delim("hotdogs.txt",header=FALSE)
+> 
+> # Summarize hotdogs
+> summary(hotdogs)
+       V1           V2              V3       
+ Beef   :20   Min.   : 86.0   Min.   :144.0  
+ Meat   :17   1st Qu.:132.0   1st Qu.:362.5  
+ Poultry:17   Median :145.0   Median :405.0  
+              Mean   :145.4   Mean   :424.8  
+              3rd Qu.:172.8   3rd Qu.:503.5  
+              Max.   :195.0   Max.   :645.0
+              
+# (2)
+> # Path to the hotdogs.txt file: path
+> path <- file.path("data", "hotdogs.txt")
+> 
+> # Import the hotdogs.txt file: hotdogs
+> hotdogs <- read.table(path, 
+                        sep = "\t", 
+                        col.names = c("type", "calories", "sodium"))
+> 
+> # Call head() on hotdogs
+> head(hotdogs)
+  type calories sodium
+1 Beef      186    495
+2 Beef      181    477
+3 Beef      176    425
+4 Beef      149    322
+5 Beef      184    482
+6 Beef      190    587
+> 
