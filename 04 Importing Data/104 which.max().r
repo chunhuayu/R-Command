8051381dcaaeb8653fcffc16d9f9147234b0ca4e
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-06-28 23:03:12 -0400

    Create 104 which.max().r

---
+++ 04 Importing Data/104 which.max().r
@@ -0,0 +1,36 @@
+### Arguments
+
+# Lily and Tom are having an argument because they want to share a hot dog but they can't seem to agree on which one to choose.
+# After some time, they simply decide that they will have one each. 
+# Lily wants to have the one with the fewest calories while Tom wants to have the one with the most sodium.
+
+# Next to calories and sodium, the hotdogs have one more variable: type. 
+# This can be one of three things: Beef, Meat, or Poultry, so a categorical variable: a factor is fine.
+
+### Instructions
+
+# Finish the read.delim() call to import the data in "hotdogs.txt". It's a tab-delimited file without names in the first row.
+# The code that selects the observation with the lowest calorie count and stores it in the variable lily is already available.
+# ......It uses the function which.min(), that returns the index the smallest value in a vector.
+# Do a similar thing for Tom: select the observation with the most sodium and store it in tom. Use which.max() this time.
+# Finally, print both the observations lily and tom.
+
+### R
+
+> # Finish the read.delim() call
+> hotdogs <- read.delim("hotdogs.txt", header = FALSE, col.names = c("type", "calories", "sodium"))
+> 
+> # Select the hot dog with the least calories: lily
+> lily <- hotdogs[which.min(hotdogs$calories), ]
+> 
+> # Select the observation with the most sodium: tom
+> tom= hotdogs[which.max(hotdogs$sodium),]
+> 
+> # Print lily and tom
+> lily
+      type calories sodium
+50 Poultry       86    358
+> tom
+   type calories sodium
+15 Beef      190    645
+> 
